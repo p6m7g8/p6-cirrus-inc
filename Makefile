df772a07c1314f6047853ef3aca6c3b193e756fc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-05 14:38:52 -0400

    chore(cicd): drops Makefile (#19)

--- Makefile
+++
@@ -1,2 +0,0 @@
-ROOT_DIR_REL?=  ../../p6/p6m7g8
-include ../../p6/p6m7g8/p6common/Makefile.inc
--- init.zsh
+++ init.zsh
@@ -1,16 +1,6 @@
 ######################################################################
 #<
 #
-# Function: p6df::modules::p6cirrus::deps()
-#
-#  Depends:	 p6_bootstrap
-#>
-######################################################################
-p6df::modules::p6cirrus::deps() { ModuleDeps=(p6m7g8/p6types) }
-
-######################################################################
-#<
-#
 # Function: p6df::modules::p6cirrus::init()
 #
 #  Depends:	 p6_bootstrap
@@ -19,7 +9,7 @@ p6df::modules::p6cirrus::deps() { ModuleDeps=(p6m7g8/p6types) }
 ######################################################################
 p6df::modules::p6cirrus::init() {
 
-  local dir="$P6_DFZ_SRC_ORIGINAL_DIR/p6-cirrus"
+  local dir="$P6_DFZ_SRC_P6M7G8_DIR/p6cirrus"
 
   p6_bootstrap "$dir"
 }
